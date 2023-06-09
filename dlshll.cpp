b76bb3af312fb30fa3c87d763e5d00454c815787
Keillion <735699921@qq.com>
2023-06-09 11:37:26 +0800

    need delete; it's better offer destroy func in HLL

--- dlshll.cpp
+++ dlshll.cpp
@@ -10,11 +10,14 @@
 
 DLSHLL_EXPORT void dlsUpdateHll(int precision, uint8_t* pBytesRegisters,
                                 uint64_t hash) {
-	auto hll = libcount::HLL::Create(precision, pBytesRegisters);
-	hll->Update(hash);
+  auto hll = libcount::HLL::Create(precision, pBytesRegisters);
+  hll->Update(hash);
+  delete hll;
 }
 DLSHLL_EXPORT uint64_t dlsEstimateHll(int precision, uint8_t* pBytesRegisters) {
-	auto hll = libcount::HLL::Create(precision, pBytesRegisters);
-	return hll->Estimate();
+  auto hll = libcount::HLL::Create(precision, pBytesRegisters);
+  auto ret = hll->Estimate();
+  delete hll;
+  return ret;
 }
 
